a968298647dc4c1eda51b2be627e7f338a42234b
solidsully <cmsully23@gmail.com>
2015-08-20 18:25:30 -0400

    worked on board class.

--- TicTacToe/TicTacToe/Board.h
+++ TicTacToe/TicTacToe/Board.h
@@ -1,1 +1,83 @@
-//board
+//board class
+class board
+{
+private: 
+	char board[3][3];
+public:
+	void getmove(int move);
+	void toggleplayer(char player);
+	bool determineDraw();
+	void drawboard();
+	void printboard();
+};
+
+void board::drawboard()
+{
+	char board[3][3] =
+	{
+		{ '1','2','3' },
+		{ '4','5','6' },
+		{ '7','8','9' },
+	};
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			cout << board[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
+void board::getmove(int move)
+{
+	char player = 'X';
+	cout << "please enter the number on the"
+		"empty space in which you'd like to move to"
+		<< endl;
+	cin >> move;
+
+	if (move == 1)
+	{
+		board[0][0] = player;
+
+	}
+	else if (move == 2)
+	{
+		board[0][1] = player;
+	}
+	else if (move == 3)
+	{
+		board[0][2] = player;
+	}
+	else if (move == 4)
+	{
+		board[1][0] = player;
+	}
+	else if (move == 5)
+	{
+		board[1][1] = player;
+	}
+	else if (move == 6)
+	{
+		board[1][2] = player;
+	}
+	else if (move == 7)
+	{
+		board[2][0] = player;
+	}
+	else if (move == 8)
+	{
+		board[2][1] = player;
+	}
+	else if (move == 9)
+	{
+		board[2][2] = player;
+	}
+	else
+	{
+		cout << "invalid move"
+			<< endl;
+	}
+}
+
+
--- TicTacToe/TicTacToe/Game.cpp
+++ TicTacToe/TicTacToe/Game.cpp
@@ -1,19 +1,29 @@
 #include <iostream>
+#include <board.h>
+#include <Player.h>
 using namespace std;
-
-int x;
-int o;
+//declare variables
+//X and Y position
+int x = 0;
+int o = 0;
+//Keeps track of current square
+int cursquare = 0;
 int p1move;
 int p2move;
-class Board;
-int boardgame[3][3];
+
+/*instantiate the board class
+and draw the board
+*/
+
 
 int main() {
+
+
 
 	cout << "Hello and welcome to Tic Tac Toe!" 
 	"Player 1 will be X "
 	"Player 2 will be O" << endl;
-	cout << " X goes first! Please enter a position on the board from 0-8" << endl;
+	cout << " X goes first! Please enter a position on the board from 1-9" << endl;
 	cin >> p1move;
 	cout << "Your move player 2!" << endl;
 	cin >> p2move;
